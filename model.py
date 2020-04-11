ca33104a399e6f32474d8364b2db19eb2949e3f5
colsoncrim <colsonscrim@gmail.com>
2020-04-11 09:49:31 -0700

    First commit

---
+++ model.py
@@ -0,0 +1,28 @@
+import tensorflow as tf 
+from tensorflow import keras 
+import numpy as np 
+import matplotlib.pyplot as plt 
+import functools
+
+# print(tf.__version__)
+
+TRAIN_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/train.csv"
+TEST_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/eval.csv"
+
+train_file_path = tf.keras.utils.get_file("train.csv", TRAIN_DATA_URL)
+test_file_path = tf.keras.utils.get_file("eval.csv", TEST_DATA_URL)
+
+np.set_printoptions(precision=3, suppress=True)
+
+
+
+# encoder = info.features['text'].encoder
+
+# print('Vocabulary size: {}'.format(encoder.vocab_size))
+
+
+
+
+
+
+
---
+++ navigate_to_card_form.py
@@ -0,0 +1,7 @@
+
+
+dataset = tf.data.TextLineDataset(["file1.txt", "file2.txt"])
+for element in dataset: 
+  print(element) 
+
+
