e0d3ebac1bcceaefbc85783078a274fcca965849
colsoncrim <colsonscrim@gmail.com>
2020-04-11 10:12:23 -0700

    Removed unnecessary files

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import functools
 
 # print(tf.__version__)
 
-TRAIN_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/train.csv"
+TRAIN_DATA_URL = "https://raw.githubusercontent.com/colsoncrim/tensorflow-python/master/navigate_to_card_form%20-%20Sheet1.csv"
 TEST_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/eval.csv"
 
 train_file_path = tf.keras.utils.get_file("train.csv", TRAIN_DATA_URL)
--- navigate_to_card_form.py
+++
@@ -1,7 +0,0 @@
-
-
-dataset = tf.data.TextLineDataset(["file1.txt", "file2.txt"])
-for element in dataset: 
-  print(element) 
-
-
